6bfe2cebcb56323f2a168a723d8e1307b2e66b9c
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-02-14 21:56:16 +0100

    Modificado AppFixtures con relaciones

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoriaFactory;
+use App\Factory\ClienteFactory;
+use App\Factory\EmpleadoFactory;
+use App\Factory\ProductoFactory;
+use App\Factory\VentaFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +14,37 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $gerente = EmpleadoFactory::new()->create([
+           'gerente' => true
+        ]);
+
+        $administrador = EmpleadoFactory::new()->create([
+            'administrador' => true
+        ]);
+
+        EmpleadoFactory::createMany(2);
+
+        ClienteFactory::createMany(50);
+
+        CategoriaFactory::createMany(10, function() use ($gerente, $administrador) {
+           return [
+               'creadaPor' => CategoriaFactory::faker()->boolean(75) ? $gerente : $administrador
+           ];
+        });
+
+        ProductoFactory::createMany(50, function() {
+           return [
+               'categoria' => CategoriaFactory::random()
+           ];
+        });
+
+        VentaFactory::createMany(100, function() {
+            return [
+                'cliente' => ClienteFactory::random(),
+                'productos' => ProductoFactory::randomRange(1, 10),
+                'empleado' => ClienteFactory::random()
+            ];
+        });
 
         $manager->flush();
     }
