4003e1c603e0fb5afc9a0434a04eb05bb1a33dde
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-20 19:40:34 +0100

    Creads rutas y metodos para el mantenimiento de productos

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Categoria;
 use App\Entity\Producto;
-use App\Factory\CategoriaFactory;
+use App\Form\ProductoType;
 use App\Repository\ProductoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,6 +32,53 @@ class ProductoController extends AbstractController
         return $this->render('producto/categoria.html.twig', [
             'productos' => $productos,
             'categoria' => $categoria->getNombre()
+        ]);
+    }
+
+    /**
+     * @Route("/producto/modificar/{id}", name="producto_modificar")
+     */
+    public function modificarProducto(Request $request, ProductoRepository $productoRepository, Producto $producto) {
+        $form = $this->createForm(ProductoType::class, $producto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $productoRepository->guardar();
+                return $this->redirectToRoute('listadoProductos');
+            } catch (\Exception $exception) {
+
+            }
+        }
+
+        return $this->render('producto/modificar.html.twig', [
+            'producto' => $producto,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/producto/nuevo", name="producto_nuevo")
+     */
+    public function nuevoProducto(Request $request, ProductoRepository $productoRepository) : Response {
+        $producto = $productoRepository->nuevo();
+        return $this->modificarProducto($request, $productoRepository, $producto);
+    }
+
+    /**
+     * @Route("/trayecto/eliminar/{id}", name="trayecto_eliminar")
+     */
+    public function eliminarTrayecto(Request $request, ProductoRepository $productoRepository, Producto $producto) : Response {
+        if ($request->get('confirmar', false)) {
+            try {
+                $productoRepository->eliminar($producto);
+                return $this->redirectToRoute('listadoProductos');
+            } catch (\Exception $e) {
+
+            }
+        }
+        return $this->render('producto/eliminar.html.twig', [
+            'producto' => $producto
         ]);
     }
 }
