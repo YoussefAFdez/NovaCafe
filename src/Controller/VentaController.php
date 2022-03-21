01b79ceccc24b9aa5e69b4870cfea466c3cdef88
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-21 02:40:27 +0100

    Creado controlador con metodos de mantenimiento y listado

---
+++ src/Controller/VentaController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Venta;
+use App\Form\VentaType;
+use App\Repository\VentaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VentaController extends AbstractController
+{
+
+    /**
+     * @Route("/venta", name="venta_listar")
+     */
+    public function listarVentas(VentaRepository $ventaRepository) : Response {
+        $ventas = $ventaRepository->findAllOrdenados();
+        return $this->render('venta/listar.html.twig', [
+           'ventas' => $ventas
+        ]);
+    }
+
+    /**
+     * @Route("/venta/nueva", name="venta_nueva")
+     */
+    public function nuevaVenta(Request $request, VentaRepository $ventaRepository) : Response {
+        $venta = $ventaRepository->nuevo();
+        return $this->modificarVenta($request, $ventaRepository, $venta);
+    }
+
+    /**
+     * @Route("/venta/modificar/{id}", name="venta_modificar")
+     */
+    public function modificarVenta(Request $request, VentaRepository $ventaRepository, Venta $venta) {
+        $form = $this->createForm(VentaType::class, $venta);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $ventaRepository->guardar();
+                return $this->redirectToRoute('venta_listar');
+            } catch (\Exception $exception) {
+
+            }
+        }
+
+        return $this->render('venta/modificar.html.twig', [
+            'venta' => $venta,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/venta/eliminar/{id}", name="venta_eliminar")
+     */
+    public function eliminarVenta(Request $request, VentaRepository $ventaRepository, Venta $venta) : Response {
+        if ($request->get('confirmar', false)) {
+            try {
+                $ventaRepository->eliminar($venta);
+                return $this->redirectToRoute('venta_listar');
+            } catch (\Exception $e) {
+
+            }
+        }
+        return $this->render('venta/eliminar.html.twig', [
+            'venta' => $venta
+        ]);
+    }
+}
