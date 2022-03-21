2aab407261577f5b3b4ad86ae16d2a6666c0c5c0
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-21 01:25:22 +0100

    Creado repositorio de cliente con metodos de mantenimiento

---
+++ src/Repository/ClienteRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cliente;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class ClienteRepository extends ServiceEntityRepository
+{
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Cliente::class);
+    }
+
+    public function findAllOrdenados()
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT c FROM App\Entity\Cliente c ORDER BY c.nombre")
+            ->getResult();
+    }
+
+    public function guardar()
+    {
+        $this->getEntityManager()->flush();
+    }
+
+    public function nuevo()
+    {
+        $cliente = new Cliente();
+        $this->getEntityManager()->persist($cliente);
+
+        return $cliente;
+    }
+
+    public function eliminar(Cliente $cliente)
+    {
+        $this->getEntityManager()->remove($cliente);
+        $this->guardar();
+    }
+
+}
