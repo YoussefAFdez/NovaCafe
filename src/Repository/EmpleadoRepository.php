120dfa69df2838417d25eb1f58dc33edd34200cc
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-21 01:08:03 +0100

    Agregados metodos de recuperacion de datos y mantenimiento de empleados

---
+++ src/Repository/EmpleadoRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Empleado;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class EmpleadoRepository extends ServiceEntityRepository
+{
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Empleado::class);
+    }
+
+    public function findAllOrdenados()
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT e FROM App\Entity\Empleado e ORDER BY e.nombre")
+            ->getResult();
+    }
+
+    public function findVentasEmpleado(Empleado $empleado)
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT v FROM App\Entity\Venta v WHERE v.empleado = :empleado")
+            ->setParameter("empleado", $empleado)
+            ->getResult();
+    }
+
+    public function findCategoriasEmpleado(Empleado $empleado)
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT c FROM App\Entity\Categoria c WHERE c.creadaPor = :empleado")
+            ->setParameter("empleado", $empleado)
+            ->getResult();
+    }
+
+    public function guardar()
+    {
+        $this->getEntityManager()->flush();
+    }
+
+    public function nuevo()
+    {
+        $empleado = new Empleado();
+        $this->getEntityManager()->persist($empleado);
+
+        return $empleado;
+    }
+
+    public function eliminar(Empleado $empleado)
+    {
+        $this->getEntityManager()->remove($empleado);
+        $this->guardar();
+    }
+
+}
